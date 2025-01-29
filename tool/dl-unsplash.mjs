8513e2990a792dd5dc6d6de3103925a77b6539ea
fahminlb33 <fahmi@kodesiana.com>
2025-01-29 12:24:41 +0700

    feat: update unsplash embed cropping and scraper using jsdom

--- tool/dl-unsplash.mjs
+++ tool/dl-unsplash.mjs
@@ -1,4 +1,4 @@
-import extractUrls from "extract-urls";
+import { JSDOM } from "jsdom";
 import commandLineArgs from 'command-line-args'
 
 const args = commandLineArgs([
@@ -15,36 +15,29 @@ if (!args.url.startsWith("https://unsplash.com")) {
   process.exit(1);
 }
 
+// fetch the page
 const response = await fetch(args.url);
 const content = await response.text();
+const dom = new JSDOM(content);
 
-// extract URL from HTML body
-const urls = extractUrls(content).filter(
-  (x) =>
-    x.includes("https://images.unsplash.com") &&
-    x.includes("ixid") &&
-    x.includes("auto="),
-);
+// extract credit
+const creditElement = dom.window.document.querySelector('meta[property~="og:title"]')
+const credit = creditElement?.getAttribute("content") ?? "Unsplash";
 
-if (urls.length === 0) {
-  console.log("No URL detected");
+// extract image caption
+// const caption = new URL(response.url).pathname.split("/").pop(-1).split("-").slice(0, -1).join(" ");
+const caption = dom.window.document.querySelector("h1").textContent;
+
+// extract image URL
+const imageElement = dom.window.document.querySelector('meta[property~="og:image"]');
+if (!imageElement) {
+  console.error("Could not find image URL");
   process.exit(1);
 }
 
-// parse image hotlink URL
-const imageUrl = new URL(urls[0]);
+const imageUrl = new URL(imageElement.getAttribute("content"));
+const imageId = imageUrl.pathname.substring(1);
+const ixid = imageUrl.searchParams.get("ixid");
 
-// extract credit from HTML body
-const creator = /Photo by [\w ]+ on Unsplash/;
-const creatorMatches = creator.exec(content);
-const creatorCredit = creatorMatches ? creatorMatches[0] : "Unsplash";
-
-// extract image caption
-const resUrl = new URL(response.url);
-let caption = resUrl.pathname.replaceAll("/photos/", "");
-caption = caption.slice(0, caption.lastIndexOf("-"));
-caption = caption.replaceAll("-", " ");
-
-console.log(
-  `{{< unsplash "${imageUrl.pathname.substring(1)}" "${imageUrl.searchParams.get("ixid")}" "${creatorCredit}" "${caption}" >}}`,
-);
+// print the shortcode
+console.log(`{{< unsplash "${imageId}" "${ixid}" "${credit}" "${caption}" >}}`);
